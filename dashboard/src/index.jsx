514db33d001a2682536802a557a4f73ce07803f7
jhameasr <gingsucks@gmail.com>
2026-03-19 17:46:50 +0800

    Update index.jsx

--- dashboard/src/index.jsx
+++ dashboard/src/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-// insert global styles nina dwane/dion
-// import './index.css'; 
+import './index.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
